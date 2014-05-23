01269dba8127aaafa3d22287ff9721a9df999ed3
XeeD <xeedus@gmail.com>
2014-05-23 17:47:02 +0200

    Localization for Czech language

--- config/application.rb
+++ config/application.rb
@@ -34,9 +34,9 @@ module Brutto
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
 
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    # I18n
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{yml}').to_s]
+    config.i18n.default_locale = :cs
     config.encoding = 'UTF-8'
     config.time_zone = 'Prague'
   end
