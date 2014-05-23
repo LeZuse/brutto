832ad045b420066a65af7382369e9f21123377ab
XeeD <xeedus@gmail.com>
2014-05-24 01:02:44 +0200

    Custom Devise/Warden authentication strategy TODO: handle case when user's credentials are incorrect more gracefully

--- app/resources/bill_app/resource_factory.rb
+++ app/resources/bill_app/resource_factory.rb
@@ -1,14 +1,22 @@
 module BillApp
   class ResourceFactory
-    def self.resource_for(element_name, klass, agenda, user, password)
+    def self.resource_for(user, password, element_name, reflection_options = {})
+      reflection_options.reverse_merge! class_name: 'BillApp::ResourceStruct'
+
       Class.new(ActiveResource::Base) do
-        self.site = "https://#{agenda}.billapp.cz"
-        self.user = user
+        self.site = "https://#{user.agenda.name}.billapp.cz"
+        self.user = user.email
         self.password = password
         self.element_name = element_name
 
-        create_reflection :user_macro, :user, class_name: klass.to_s
+        create_reflection :user_macro, :user, reflection_options
       end
+    end
+  end
+
+  class ResourceStruct < OpenStruct
+    def initialize(attributes, persisted)
+      super(attributes)
     end
   end
 end
---
+++ lib/devise/strategies/bill_app_authenticable.rb
@@ -0,0 +1,51 @@
+module Devise
+  module Strategies
+    class BillAppAuthenticable < Devise::Strategies::Authenticatable
+      def valid?
+        params[:user].present?
+      end
+
+      def authenticate!
+        user = find_or_create_user
+        remote_user = self.find_remote_user(user)
+        user.update_attribute(:name, remote_user.name)
+
+        save_password_to_session
+
+        success! user
+      end
+
+      # Add strategy to Warden
+      Warden::Strategies.add :bill_app_authenticable, self
+
+    protected
+
+      def save_password_to_session
+        session[:bill_app_api_password] = password
+      end
+
+      def find_or_create_user
+        klass = mapping.to
+        klass.find_or_create_by(email: params[:user][:email], agenda_id: agenda.id) do |new_user|
+          new_user.password = password
+        end
+      end
+
+      def agenda
+        @agenda ||= Agenda.find_or_create_by(name: params[:user][:agenda])
+      end
+
+      def find_remote_user(user)
+        user_resource(user).all.find { |u| u.email == user.email }
+      end
+
+      def user_resource(user)
+        BillApp::ResourceFactory.resource_for(user, password, 'user')
+      end
+
+      def password
+        params[:user][:password]
+      end
+    end
+  end
+end
