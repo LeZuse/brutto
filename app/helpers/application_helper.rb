3fc9121106231e80ab43713dc5d11b19570325cd
XeeD <xeedus@gmail.com>
2014-05-26 17:38:27 +0200

    Very simple implementation of Presenter pattern

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+  def present(object)
+    yield presenter_for(object)
+  end
+
+  def present_collection(collection)
+    presenters = collection.map { |member| presenter_for(member) }
+
+    if block_given?
+      presenters.each(&block)
+    else
+      presenters
+    end
+  end
+
+  def presenter_for(object)
+    "#{object.class.to_s}Presenter".
+        constantize.
+        new(object)
+  end
 end
---
+++ app/presenters/application_presenter.rb
@@ -0,0 +1,13 @@
+class ApplicationPresenter
+  def initialize(object)
+    @object = object
+  end
+
+  def method_missing(*args, &block)
+    @object.send(*args, &block)
+  end
+
+  def respond_to_missing?(method, include_priv)
+    @object.respond_to?(method, include_priv)
+  end
+end
