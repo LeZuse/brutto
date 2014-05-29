93a586775b8d1070c7e3a735b3efc56c0d5cb56f
XeeD <xeedus@gmail.com>
2014-05-29 08:43:43 +0200

    DRY up scopes in Invoice model

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -13,6 +13,10 @@ class Invoice < ActiveRecord::Base
     order issue_date: :desc, number: :desc
   }
 
+  scope :grouped_with_lines, -> {
+    joins(:lines).group(arel_table[:id])
+  }
+
   scope :from_agenda, ->(agenda_id) {
     where agenda_id: agenda_id
   }
@@ -20,18 +24,14 @@ class Invoice < ActiveRecord::Base
   scope :with_profit, -> {
     total_profit_sum = InvoiceLine.total_profit_col.sum
 
-    joins(:lines).
-        group(arel_table[:id]).
-        select arel_table[Arel.star],
-               total_profit_sum.as('total_profit')
+    grouped_with_lines.select arel_table[Arel.star],
+                              total_profit_sum.as('total_profit')
   }
 
   scope :with_totals, -> {
-    joins(:lines).
-      group(arel_table[:id]).
-      select arel_table[Arel.star],
-             InvoiceLine.total_amount_col.sum.as('total_amount'),
-             InvoiceLine.total_tax_amount_col.sum.as('tax_amount')
+    grouped_with_lines.select arel_table[Arel.star],
+                              InvoiceLine.total_amount_col.sum.as('total_amount'),
+                              InvoiceLine.total_tax_amount_col.sum.as('tax_amount')
   }
 
   # Instance method
