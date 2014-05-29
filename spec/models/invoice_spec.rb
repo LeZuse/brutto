ef519767237b0553b1fb0e94ac6e969181927996
XeeD <xeedus@gmail.com>
2014-05-29 09:06:17 +0200

    Add example for invoice with VAT

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Invoice do
-  it 'sums up profit of line items as total_profit' do
+  it 'sums up profit of invoice lines as total_profit' do
     invoice = create(:invoice)
 
     create(:invoice_line, invoice: invoice, cost: 10, unit_price: 20, quantity: 2) # profit 2 x 10 = 20
@@ -9,9 +9,19 @@ describe Invoice do
                                                                                    # total_profit  = 35
 
 
-    invoice = Invoice.with_profit.last
-    puts invoice.total_profit.to_s
-    binding.pry
-    expect(invoice.total_profit).to eq(BigDecimal(35))
+    invoice_with_profit = Invoice.with_profit.find(invoice.id)
+    expect(invoice_with_profit.total_profit).to eq(BigDecimal(35))
+  end
+
+  it 'sums up profit of invoice lines as total_profit taking VAT into account' do
+    invoice = create(:invoice)
+
+    create(:invoice_line, invoice: invoice, cost: 80, unit_price: 121, quantity: 2, vat: 21)   # profit 2 x 20  = 40
+    create(:invoice_line, invoice: invoice, cost: 300, unit_price: 460, quantity: 4, vat: 15)  # profit 4 x 100 = 400
+                                                                                               # total_profit   = 440
+
+
+    invoice_with_profit = Invoice.with_profit.find(invoice.id)
+    expect(invoice_with_profit.total_profit).to eq(BigDecimal(440))
   end
 end
