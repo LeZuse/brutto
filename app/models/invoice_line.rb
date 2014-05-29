0995b12092d03aa8887248798bf2beace3be1a21
XeeD <xeedus@gmail.com>
2014-05-29 08:56:04 +0200

    Fix calculation of profits for invoice lines with VAT

--- app/models/invoice_line.rb
+++ app/models/invoice_line.rb
@@ -7,38 +7,57 @@ class InvoiceLine < ActiveRecord::Base
   # ======
   scope :with_profit, -> {
     order(:id).
-      select(arel_table[Arel.star], total_profit_col.as('total_profit'))
+        select(arel_table[Arel.star], total_profit_col.as('total_profit'))
   }
 
   # Class methods
   # =============
   class << self
-    # unit_price - cost
-    def profit_col
-      Arel::Nodes::Subtraction.new price_without_vat,
-                                   arel_table[:cost]
-    end
-
-    # quantity * (unit_price - cost)
-    def total_profit_col
-      arel_table[:quantity] * Arel::Nodes::Grouping.new(profit_col)
-    end
-
+    # quantity * unit_price
     def total_amount_col
       arel_table[:quantity] * arel_table[:unit_price]
     end
 
-    # quantity * unit_price * vat / 100.0
-    def tax_amount_col
-      arel_table[:unit_price] * arel_table[:vat] / Arel.sql('100.0')
+    # [unit_price_without_vat] - cost
+    # may require grouping when used as part of another calculation
+    def profit_col
+      Arel::Nodes::Subtraction.new unit_price_without_vat_col,
+                                   arel_table[:cost]
     end
 
+    # quantity * [profit]
+    def total_profit_col
+      arel_table[:quantity] * Arel::Nodes::Grouping.new(profit_col)
+    end
+
+    # unit_price - [unit_price_without_vat]
+    def tax_amount_col
+      arel_table[:unit_price] - unit_price_without_vat_col
+    end
+
+    # quantity * [tax_amount]
     def total_tax_amount_col
       arel_table[:quantity] * tax_amount_col
     end
 
-    def price_without_vat
-      arel_table[:unit_price] - tax_amount_col
+    # round(unit_price / (vat / 100.0 + 1), 2)
+    def unit_price_without_vat_col
+      rounded arel_table[:unit_price] / vat_multiplier_col
+    end
+
+    # (vat / 100.0 + 1)
+    def vat_multiplier_col
+      calculation = Arel::Nodes::Addition.new arel_table[:vat] / Arel.sql('100.0'),
+                                              Arel.sql('1')
+
+      Arel::Nodes::Grouping.new(calculation)
+    end
+
+  private
+
+    # round(..., X)
+    def rounded(arel_node, decimal_places = 2)
+      Arel::Nodes::NamedFunction.new 'round', [arel_node, decimal_places]
     end
   end
 end
--- app/presenters/invoice_line_presenter.rb
+++ app/presenters/invoice_line_presenter.rb
@@ -1,9 +1,17 @@
 class InvoiceLinePresenter < ApplicationPresenter
   def total_profit
-    (unit_price - vat_amount - cost.to_i) * quantity
+    quantity * profit
+  end
+
+  def profit
+    unit_price_without_vat - cost
   end
 
   def vat_amount
-    unit_price * vat / 100.0
+    unit_price - unit_price_without_vat
+  end
+
+  def unit_price_without_vat
+    (unit_price / (vat / 100.0 + 1)).round(2)
   end
 end
